4de59d5f2e7c55cff684401dcd02a382d4ded698
Joe Danziger <joe@ticc.net>
2026-01-29 01:23:28 -0500

    chore: update generated Convex API types for matchConfig module

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type * as dashboard from "../dashboard.js";
 import type * as http from "../http.js";
 import type * as invites from "../invites.js";
 import type * as leagues from "../leagues.js";
+import type * as matchConfig from "../matchConfig.js";
 import type * as members from "../members.js";
 import type * as onboarding from "../onboarding.js";
 
@@ -30,6 +31,7 @@ declare const fullApi: ApiFromModules<{
   http: typeof http;
   invites: typeof invites;
   leagues: typeof leagues;
+  matchConfig: typeof matchConfig;
   members: typeof members;
   onboarding: typeof onboarding;
 }>;
